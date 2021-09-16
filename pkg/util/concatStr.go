f4666d6d293210193cbe487e0ebac62b8ab5dda8
Lucareful <34530552+Lucareful@users.noreply.github.com>
2021-09-16 14:02:03 +0800

    update: 修复字符串拼接产生的内存复制

--- pkg/util/concatStr.go
+++ pkg/util/concatStr.go
@@ -4,11 +4,18 @@ import "strings"
 
 // ConcatStr 字符串拼接
 func ConcatStr(str ...string) string {
-	var builder strings.Builder
-	builder.Grow(len(str))
-	for _, v := range str {
-		builder.WriteString(v)
+	var (
+		builder strings.Builder
+		n       int
+	)
+	for i := 0; i < len(str); i++ {
+		n += len(str[i])
 	}
-
+	builder.Grow(n)
+	// 直接写值的时候，str发生复制
+	// 索引取值可减少内存复制
+	for i := range str {
+		builder.WriteString(str[i])
+	}
 	return builder.String()
 }
