39d54ba4dcfda8ae9eb27c52ef0f938d40231855
Lusheng <sheng.lu@mail.hypers.com>
2021-10-12 22:07:38 +0800

    feat: 增加自定义WithResponse

--- go.mod
+++ go.mod
@@ -25,7 +25,7 @@ require (
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.0.0-20190425150028-36563e24a262
+	golang.org/x/tools v0.1.7
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.47.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- pkg/http/render/WriteResponse.go
+++ pkg/http/render/WriteResponse.go
@@ -1,7 +1,9 @@
 package render
 
 import (
-	"net/http"
+	"sync"
+
+	"github.com/luenci/go-gin-example/pkg/e"
 
 	"github.com/gin-gonic/gin"
 	"github.com/marmotedu/errors"
@@ -9,21 +11,56 @@ import (
 
 // Response 不分页的返回响应结构体
 type Response struct {
-	Code    int                    `json:"code,omitempty"`
-	Message string                 `json:"message,omitempty"`
-	Data    map[string]interface{} `json:"data,omitempty"`
+	Code    int         `json:"code,omitempty"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func (r *Response) reset() {
+	r.Data = nil
+	r.Message = ""
+	r.Code = 0
+}
+
+// 请求结构体复用
+var responsePool = &sync.Pool{
+	New: func() interface{} {
+		return new(Response)
+	},
 }
 
 // WriteResponse 返回响应的数据.
-func WriteResponse(c *gin.Context, data ...interface{}) {
+func WriteResponse(c *gin.Context, err error, data interface{}) {
+
+	response := responsePool.Get().(*Response)
+	defer func() {
+		response.reset()
+		responsePool.Put(response)
+	}()
+
+	var httpStatus int
+
 	if err != nil {
 		coder := errors.ParseCoder(err)
-		c.JSON(coder.HTTPStatus(), Response{
-			Code:    coder.Code(),
-			Message: coder.String(),
-			Data:    data,
-		})
+		response.Code = coder.Code()
+		response.Message = coder.String()
+		httpStatus = coder.HTTPStatus()
+	} else {
+		response.Code = e.SUCCESS
+		response.Message = "Success"
+		httpStatus = 200
 	}
 
-	c.JSON(http.StatusOK, Response{Data: data})
+	switch {
+	case httpStatus >= 400 && httpStatus < 500:
+		c.Abort()
+		c.Set("warn", err)
+		response.Data = err.Error()
+	case httpStatus >= 500:
+		c.Abort()
+		c.Set("error", err)
+	default:
+		response.Data = data
+	}
+	c.JSON(httpStatus, response)
 }
